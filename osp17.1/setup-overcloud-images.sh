cf1758e41b43ced49e393f094e4358acbaeeb931
ariveamar <41986809+ariveamar@users.noreply.github.com>
2024-06-16 16:57:34 +0700

    Update setup-overcloud-images.sh

--- osp17.1/setup-overcloud-images.sh
+++ osp17.1/setup-overcloud-images.sh
@@ -1,1 +1,9 @@
-
+#!/bin/bash
+su - stack
+source ~/stackrc
+sudo dnf install rhosp-director-images-uefi-x86_64 rhosp-director-images-ipa-x86_64
+mkdir /home/stack/images
+mkdir /home/stack/templates
+cd /home/stack/images
+for i in /usr/share/rhosp-director-images/ironic-python-agent-latest.tar /usr/share/rhosp-director-images/overcloud-hardened-uefi-full-latest.tar; do tar -xvf $i; done
+openstack overcloud image upload --image-path /home/stack/images/
