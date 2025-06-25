da107f17d2ece1542945932e44cae6daaaee11e3
ariveamar <41986809+ariveamar@users.noreply.github.com>
2025-06-25 10:18:39 +0700

    Create initial-setup-ansible.sh

---
+++ ansible/initial-setup-ansible.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+dnf module -y install postgresql:15
+dnf install postgresql-contrib -y
+postgresql-setup --initdb
+systemctl enable --now postgresql
+sed -i "s/#listen_addresses = 'localhost'/listen_addresses = '*'/" /var/lib/pgsql/data/postgresql.conf ;
+echo "host      all    all    0.0.0.0/0 md5" >> /var/lib/pgsql/data/pg_hba.conf ;
+sudo -u postgres  createuser automation;
+sudo -u postgres  createdb automationhub -O automation ;
+
+
+sudo -u postgres psql -d automationhub -c "CREATE EXTENSION hstore;"  ;
+sudo -u postgres psql -c "ALTER ROLE automation WITH SUPERUSER;" ;
+systemctl restart postgresql
